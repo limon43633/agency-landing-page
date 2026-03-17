21f8d315b957c8706d2ddd2df4d909a4f2412661
Hassibur Rahman <md9178558@gmail.com>
2026-03-18 00:02:00 +0600

    added teams

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
 import Services from './components/Services';
+import OurWork from './components/OurWork';
+import Teams from './components/Teams';
 
 
 
@@ -17,6 +19,8 @@ const App = () => {
       <Hero />
       <TrustedBy />
       <Services />
+      <OurWork />
+      <Teams />
       
     </div>
   );
