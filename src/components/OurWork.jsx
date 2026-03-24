52c0c4f8967c3961fa2283d6eb63e6a44a1a1bf3
Hassibur Rahman <md9178558@gmail.com>
2026-03-24 18:30:42 +0600

    final commit

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from './Title';
 import assets from '../assets/assets';
+import { motion } from 'motion/react';
 
 const OurWork = () => {
 
@@ -23,22 +24,35 @@ const OurWork = () => {
     ]
 
     return (
-        <div id='our-work' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ staggerChildren: 0.2 }}
+
+
+         id='our-work' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
             <Title title='Our latest work' desc='Browse our portfolio of innovative digital projects that showcase creativity, performance, and results.' />
 
             <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl'>
                 {
                     OurWork.map((work, index)=>(
-                        <div key={index} className='hover:scale-102 duration-500 transition-all cursor-pointer'>
+                        <motion.div 
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, delay: index * 0.2 }}
+                        viewport={{ once: true }}
+                        
+                        key={index} className='hover:scale-102 duration-500 transition-all cursor-pointer'>
                             <img src={work.image} alt="" className='w-full rounded-xl ' />
                             <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
                             <p className='text-sm opacity-60 w-5/6'>{work.description}</p>
-                        </div>
+                        </motion.div>
                     ))
                 }
             </div>
 
-        </div>
+        </motion.div>
     );
 };
 
